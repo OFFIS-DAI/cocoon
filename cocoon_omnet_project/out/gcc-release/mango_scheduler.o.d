6654f45e1db26bb8f60ba472768a1ce75d2072d2
Malin <malin.radtke@uni-oldenburg.de>
2025-05-19 12:52:15 +0200

    working version

---
+++ cocoon_omnet_project/out/gcc-release/mango_scheduler.o.d
@@ -0,0 +1,6 @@
+out/gcc-release//mango_scheduler.o: mango_scheduler.cc mango_scheduler.h \
+ /home/malin/omnetpp-6.0.1/include/omnetpp.h
+
+mango_scheduler.h:
+
+/home/malin/omnetpp-6.0.1/include/omnetpp.h:
