f73c857cdbd275c6e9ebce66a30c5ff9d19f36c3
Malin <malin.radtke@uni-oldenburg.de>
2025-06-18 13:24:04 +0200

    Further development of comparison script.

--- integration_environment/model_comparison/execute_comparison.py
+++ integration_environment/model_comparison/execute_comparison.py
@@ -1,9 +1,11 @@
 import asyncio
 import logging
+import os
 import random
 import time
 from typing import Dict, Optional
 
+import pandas as pd
 from mango import agent_composed_of, JSON, activate, ExternalClock
 from mango.container.external_coupling import ExternalSchedulingContainer
 from mango.container.factory import create_external_coupling
@@ -34,6 +36,52 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+def get_training_df(scenario_configuration: ScenarioConfiguration):
+    existing_configurations = [ScenarioConfiguration.from_scenario_id(f.split('.')[0])
+                               for f in os.listdir('cocoon_training_data')]
+    different_traffic_configs = [c for c in existing_configurations
+                                 if c.traffic_configuration != scenario_configuration.traffic_configuration]
+    dataframes = []
+    for c in different_traffic_configs:
+        dataframes.append(pd.read_csv(f'cocoon_training_data/{c.scenario_id}.csv'))
+    return pd.concat(dataframes)
+
+
+def get_scenario_configurations_for_meta_model_training():
+    existing_configuration_ids = [f.split('.')[0] for f in os.listdir('cocoon_training_data')]
+    scenario_configurations = []
+    for network in [NetworkModelType.simbench_lte450, NetworkModelType.simbench_ethernet,
+                    NetworkModelType.simbench_lte, NetworkModelType.simbench_5g]:
+        for payload_size in [  # PayloadSizeConfig.small,
+            PayloadSizeConfig.medium,
+            # PayloadSizeConfig.large
+        ]:
+            for scenario_duration in [ScenarioDuration.one_min,
+                                      # ScenarioDuration.five_min,
+                                      # ScenarioDuration.one_hour
+                                      ]:
+                for n_devices in [NumDevices.ten]:  # , NumDevices.two, NumDevices.fifty, NumDevices.hundred]:
+                    for traffic_config in [TrafficConfig.cbr_broadcast_1_mps,
+                                           # TrafficConfig.cbr_broadcast_1_mpm,
+                                           # TrafficConfig.cbr_broadcast_4_mph,
+                                           TrafficConfig.poisson_broadcast_1_mps,
+                                           # TrafficConfig.poisson_broadcast_1_mpm,
+                                           # TrafficConfig.poisson_broadcast_4_mph,
+                                           TrafficConfig.unicast_1s_delay,
+                                           # TrafficConfig.unicast_5s_delay,
+                                           # TrafficConfig.unicast_10s_delay,
+                                           TrafficConfig.deer_use_case]:
+                        config = ScenarioConfiguration(payload_size=payload_size,
+                                                       num_devices=n_devices,
+                                                       model_type=ModelType.meta_model_training,
+                                                       scenario_duration=scenario_duration,
+                                                       traffic_configuration=traffic_config,
+                                                       network_type=network)
+                        if config.scenario_id not in existing_configuration_ids:
+                            scenario_configurations.append(config)
+    return scenario_configurations
+
+
 def get_scenario_configurations():
     scenario_configurations = []
     for network in [NetworkModelType.simbench_lte450, NetworkModelType.simbench_ethernet,
@@ -44,13 +92,16 @@ def get_scenario_configurations():
                 for scenario_duration in [ScenarioDuration.one_min, ScenarioDuration.five_min,
                                           ScenarioDuration.one_hour]:
                     for n_devices in [NumDevices.ten]:  # , NumDevices.two, NumDevices.fifty, NumDevices.hundred]:
-                        for traffic_config in [#TrafficConfig.cbr_broadcast_1_mps, TrafficConfig.cbr_broadcast_1_mpm,
-                                               #TrafficConfig.cbr_broadcast_4_mph,
-                                               #TrafficConfig.poisson_broadcast_1_mps,
-                                               #TrafficConfig.poisson_broadcast_1_mpm,
-                                               #TrafficConfig.poisson_broadcast_4_mph,
-                                               TrafficConfig.unicast_1s_delay, TrafficConfig.unicast_5s_delay,
-                                               TrafficConfig.unicast_10s_delay, TrafficConfig.deer_use_case]:
+                        for traffic_config in [TrafficConfig.cbr_broadcast_1_mps,
+                                               TrafficConfig.cbr_broadcast_1_mpm,
+                                               TrafficConfig.cbr_broadcast_4_mph,
+                                               TrafficConfig.poisson_broadcast_1_mps,
+                                               TrafficConfig.poisson_broadcast_1_mpm,
+                                               TrafficConfig.poisson_broadcast_4_mph,
+                                               TrafficConfig.unicast_1s_delay,
+                                               TrafficConfig.unicast_5s_delay,
+                                               TrafficConfig.unicast_10s_delay,
+                                               TrafficConfig.deer_use_case]:
                             scenario_configurations.append(ScenarioConfiguration(payload_size=payload_size,
                                                                                  num_devices=n_devices,
                                                                                  model_type=model_type,
@@ -88,7 +139,18 @@ def get_scheduler(scenario_configuration: ScenarioConfiguration,
                                   config_name=scenario_configuration.network_type.value,
                                   inet_installation_path='/home/malin/cocoon_omnet_workspace/inet4.5/src',
                                   simu5G_installation_path='/home/malin/PycharmProjects/trace/Simu5G-1.2.2/src',
-                                  omnet_project_path='/home/malin/PycharmProjects/cocoon_DAI/cocoon_omnet_project/'
+                                  omnet_project_path='/home/malin/PycharmProjects/cocoon_DAI/cocoon_omnet_project/',
+                                  training_df=get_training_df(scenario_configuration)
+                                  )
+    elif scenario_configuration.model_type == ModelType.meta_model_training:
+        return MetaModelScheduler(container_mapping=container_mapping,
+                                  scenario_duration_ms=scenario_configuration.scenario_duration.value,
+                                  config_name=scenario_configuration.network_type.value,
+                                  inet_installation_path='/home/malin/cocoon_omnet_workspace/inet4.5/src',
+                                  simu5G_installation_path='/home/malin/PycharmProjects/trace/Simu5G-1.2.2/src',
+                                  omnet_project_path='/home/malin/PycharmProjects/cocoon_DAI/cocoon_omnet_project/',
+                                  in_training_mode=True,
+                                  output_file_name=f'cocoon_training_data/{scenario_configuration.scenario_id}.csv'
                                   )
     else:
         logging.warning(f'Unknown model type: {scenario_configuration.model_type}')
@@ -151,7 +213,7 @@ async def initialize_unicast_communication_agents(clock: ExternalClock,
                                                   results_recorder: ResultsRecorder,
                                                   scenario_configuration: ScenarioConfiguration):
     container_mapping = {}
-    receiver_addresses = []
+    receiver_addr = []
 
     agents = []
     for n_agents in range(scenario_configuration.num_devices.value):
@@ -160,14 +222,14 @@ async def initialize_unicast_communication_agents(clock: ExternalClock,
         receiver_role = ReceiverRole()
         agent = agent_composed_of(receiver_role, ResultsRecorderRole(results_recorder))
         container.register(agent)
-        receiver_addresses.append(agent.addr)
+        receiver_addr.append(agent.addr)
         container_mapping[f'node{index}'] = container
         agents.append((container, agent))
 
     # Second pass: Add UnicastRole to each agent with addresses of all OTHER agents
     for i, (container, agent) in enumerate(agents):
         # Get receiver addresses (all agents except this one)
-        receiver_addresses = [addr for j, addr in enumerate(receiver_addresses) if j != i]
+        receiver_addresses = [addr for j, addr in enumerate(receiver_addr) if j != i]
 
         # Add UnicastRole to the existing agent
         unicast_role = UnicastRole(receiver_addresses=receiver_addresses,
@@ -229,7 +291,17 @@ async def run_scenario(container_mapping: Dict[str, ExternalSchedulingContainer]
 
 
 async def run_benchmark_suite():
-    for scenario_configuration in get_scenario_configurations():
+    # clean up result folder first
+    for f in [f for f in os.listdir('results')]:
+        os.remove(os.path.join('results', f))
+
+    meta_model_training_configs = get_scenario_configurations_for_meta_model_training()
+    evaluation_configs = get_scenario_configurations()
+
+    print(f'Start running {len(meta_model_training_configs)} scenarios for meta-model training. '
+          f'Afterwards, {len(evaluation_configs)} scenarios for model comparison will be executed. ')
+
+    for scenario_configuration in (meta_model_training_configs + evaluation_configs):
         results_recorder = ResultsRecorder(scenario_configuration=scenario_configuration)
         clock = ExternalClock(start_time=0)
 
