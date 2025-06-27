476a522558ef0996660fd7bae5503fccca7fdda9
Malin <malin.radtke@uni-oldenburg.de>
2025-06-27 09:07:24 +0200

    Adjust scenario to current implementation.

--- tests/integration_tests/test_cbr_scenario.py
+++ tests/integration_tests/test_cbr_scenario.py
@@ -10,10 +10,11 @@ from mango.container.factory import create_external_coupling
 from integration_environment.communication_model_scheduler import IdealCommunicationScheduler, ChannelModelScheduler, \
     StaticDelayGraphModelScheduler, DetailedModelScheduler, MetaModelScheduler
 from integration_environment.messages import TrafficMessage
+from integration_environment.model_comparison.execute_comparison import get_training_df, \
+    initialize_constant_bitrate_broadcast_agents
 from integration_environment.results_recorder import ResultsRecorder
 from integration_environment.roles import ConstantBitrateSenderRole, ReceiverRole, ResultsRecorderRole
-from integration_environment.scenario_configuration import ScenarioConfiguration, PayloadSizeConfig, ModelType, \
-    ScenarioDuration, NetworkModelType, NumDevices
+from integration_environment.scenario_configuration import *
 from tests.integration_tests.utils import setup_logging, visualize_channel_model_graph, visualize_static_graph
 
 logger = setup_logging()
@@ -133,140 +134,75 @@ async def run_scenario_with_static_graph_model():
     assert len(cbr_receiver_role.received_messages) > 0
 
 
-async def run_scenario_with_detailed_communication_simulation():
-    scenario_configuration = ScenarioConfiguration(model_type=ModelType.detailed,
-                                                   network_type=NetworkModelType.simbench_lte450,
-                                                   num_devices=NumDevices.ten)
-    results_recorder = ResultsRecorder(scenario_configuration=scenario_configuration)
+@pytest.mark.asyncio
+async def test_run_scenario_with_detailed_communication_simulation():
+    for run in range(3):
+        scenario_configuration = ScenarioConfiguration(payload_size=PayloadSizeConfig.small,
+                                                       num_devices=NumDevices.two,
+                                                       model_type=ModelType.detailed,
+                                                       scenario_duration=ScenarioDuration.one_min,
+                                                       traffic_configuration=TrafficConfig.cbr_broadcast_1_mps,
+                                                       network_type=NetworkModelType.simbench_5g,
+                                                       run=run)
+        results_recorder = ResultsRecorder(scenario_configuration=scenario_configuration)
 
-    clock = ExternalClock(start_time=0)
+        clock = ExternalClock(start_time=0)
 
-    container_mapping = {}
+        container_mapping = await initialize_constant_bitrate_broadcast_agents(
+            scenario_configuration=scenario_configuration,
+            results_recorder=results_recorder,
+            clock=clock)
 
-    for i in range(scenario_configuration.num_devices.value):
-        container = create_external_coupling(addr=f'node{i}', codec=my_codec, clock=clock)
-        container_mapping[f'node{i}'] = container
+        communication_network_entity = DetailedModelScheduler(container_mapping=container_mapping,
+                                                              inet_installation_path='/home/malin/cocoon_omnet_workspace/inet4.5/src',
+                                                              simu5G_installation_path='/home/malin/PycharmProjects/trace/Simu5G-1.2.2/src',
+                                                              config_name=scenario_configuration.network_type.value,
+                                                              omnet_project_path='/home/malin/PycharmProjects/cocoon_DAI/cocoon_omnet_project/',
+                                                              scenario_duration_ms=scenario_configuration.scenario_duration.value,
+                                                              )
 
-    communication_network_entity = DetailedModelScheduler(container_mapping=container_mapping,
-                                                          inet_installation_path='/home/malin/cocoon_omnet_workspace/inet4.5/src',
-                                                          config_name=scenario_configuration.network_type.value,
-                                                          simu5G_installation_path='/home/malin/PycharmProjects/trace/Simu5G-1.2.2/src',
-                                                          omnet_project_path='/home/malin/PycharmProjects/cocoon_DAI/cocoon_omnet_project/')
-
-    recv_addr = []
-    for i in range(scenario_configuration.num_devices.value - 1):
-        cbr_receiver_role = ReceiverRole()
-        cbr_receiver_role_agent = agent_composed_of(cbr_receiver_role, ResultsRecorderRole(results_recorder))
-        list(container_mapping.values())[i].register(cbr_receiver_role_agent)
-        recv_addr.append(cbr_receiver_role_agent.addr)
-
-    sender_container = list(container_mapping.values())[scenario_configuration.num_devices.value - 1]
-    sender_container.current_start_time_of_step = time.time()
-    cbr_sender_role_agent = agent_composed_of(
-        ConstantBitrateSenderRole(receiver_addresses=recv_addr,
-                                  scenario_config=scenario_configuration),
-        ResultsRecorderRole(results_recorder))
-    sender_container.register(cbr_sender_role_agent)
-
-    async with activate(list(container_mapping.values())) as _:
-        results_recorder.start_scenario_recording()
-        await communication_network_entity.scenario_finished
-    results_recorder.stop_scenario_recording()
-
-
-async def run_scenario_with_meta_model():
-    # first, we run a scenario with 2 devices in training mode
-    """scenario_configuration = ScenarioConfiguration(scenario_id='meta-model-training', payload_size=PayloadSizeConfig.SMALL,
-                                                   num_devices=2)
-    training_data_file = f'results/cocoon_message_observations/{scenario_configuration.scenario_id}.csv'
-    results_recorder = ResultsRecorder(scenario_configuration=scenario_configuration)
-
-    clock = ExternalClock(start_time=0)
-
-    container1 = create_external_coupling(addr='node1', codec=my_codec, clock=clock)
-    container2 = create_external_coupling(addr='node2', codec=my_codec, clock=clock)
-
-    communication_network_entity = (output_file_name
-        MetaModelScheduler(container_mapping={'node1': container1,
-                                              'node2': container2},
-                           inet_installation_path='/home/malin/cocoon_omnet_workspace/inet4.5/src',
-                           config_name='General',
-                           omnet_project_path='/home/malin/PycharmProjects/cocoon_DAI/cocoon_omnet_project/',
-                           in_training_mode=True,
-                           output_file_name=training_data_file))
-
-    cbr_receiver_role = ConstantBitrateReceiverRole()
-    cbr_receiver_role_agent = agent_composed_of(cbr_receiver_role, ResultsRecorderRole(results_recorder))
-    container1.register(cbr_receiver_role_agent)
-
-    container2.current_start_time_of_step = time.time()
-    cbr_sender_role_agent = agent_composed_of(
-        ConstantBitrateSenderRole(receiver_addresses=[cbr_receiver_role_agent.addr],
-                                  scenario_config=scenario_configuration),
-        ResultsRecorderRole(results_recorder))
-    container2.register(cbr_sender_role_agent)
-
-    async with activate(container1, container2) as _:
-        results_recorder.start_scenario_recording()
-        await communication_network_entity.scenario_finished
-    results_recorder.stop_scenario_recording()
-
-    assert len(cbr_receiver_role.received_messages) > 0"""
-
-    training_data_file = 'results/cocoon_message_observations/meta-model-training.csv'
-    # now, we want to conduct the production scenario with 3 devices
-    training_df = pd.read_csv(training_data_file)
-
-    scenario_configuration = ScenarioConfiguration(model_type=ModelType.meta_model)
-
-    results_recorder = ResultsRecorder(scenario_configuration=scenario_configuration)
-
-    clock = ExternalClock(start_time=0)
-
-    container1 = create_external_coupling(addr='node1', codec=my_codec, clock=clock)
-    container2 = create_external_coupling(addr='node2', codec=my_codec, clock=clock)
-    container3 = create_external_coupling(addr='node3', codec=my_codec, clock=clock)
-
-    communication_network_entity = (
-        MetaModelScheduler(container_mapping={'node1': container1,
-                                              'node2': container2},
-                           inet_installation_path='/home/malin/cocoon_omnet_workspace/inet4.5/src',
-                           simu5G_installation_path='/home/malin/PycharmProjects/trace/Simu5G-1.2.2/src',
-                           config_name='Ethernet',
-                           omnet_project_path='/home/malin/PycharmProjects/cocoon_DAI/cocoon_omnet_project/',
-                           in_training_mode=False,
-                           training_df=training_df,
-                           cluster_distance_threshold=0.1,
-                           output_file_name='results/cocoon_message_observations/production.csv'))
-
-    cbr_receiver_role = ReceiverRole()
-    cbr_receiver_role_agent = agent_composed_of(cbr_receiver_role, ResultsRecorderRole(results_recorder))
-    container1.register(cbr_receiver_role_agent)
-
-    container2.current_start_time_of_step = time.time()
-    cbr_sender_role_agent = agent_composed_of(
-        ConstantBitrateSenderRole(receiver_addresses=[cbr_receiver_role_agent.addr],
-                                  scenario_config=scenario_configuration),
-        ResultsRecorderRole(results_recorder))
-    container2.register(cbr_sender_role_agent)
-
-    container3.current_start_time_of_step = time.time()
-    cbr_sender_role_agent = agent_composed_of(
-        ConstantBitrateSenderRole(receiver_addresses=[cbr_receiver_role_agent.addr],
-                                  scenario_config=scenario_configuration),
-        ResultsRecorderRole(results_recorder))
-    container3.register(cbr_sender_role_agent)
-
-    async with activate(container1, container2, container3) as _:
-        results_recorder.start_scenario_recording()
-        await communication_network_entity.scenario_finished
-    results_recorder.stop_scenario_recording()
+        async with activate([c for c in container_mapping.values()]) as _:
+            results_recorder.start_scenario_recording()
+            await communication_network_entity.scenario_finished
+        results_recorder.stop_scenario_recording()
 
 
 @pytest.mark.asyncio
-def test_scenarios():
-    asyncio.run(run_scenario_with_ideal_communication())
-    asyncio.run(run_scenario_with_simple_channel_model())
-    asyncio.run(run_scenario_with_static_graph_model())
-    asyncio.run(run_scenario_with_detailed_communication_simulation())
-    asyncio.run(run_scenario_with_meta_model())
+async def test_run_scenario_with_meta_model():
+    scenario_configuration = ScenarioConfiguration(payload_size=PayloadSizeConfig.small,
+                                                   num_devices=NumDevices.two,
+                                                   model_type=ModelType.meta_model,
+                                                   scenario_duration=ScenarioDuration.one_min,
+                                                   traffic_configuration=TrafficConfig.cbr_broadcast_1_mps,
+                                                   network_type=NetworkModelType.simbench_5g,
+                                                   cluster_distance_threshold=ClusterDistanceThreshold.half,
+                                                   i_pupa=BatchSizeIPupa.ten)
+
+    training_df = get_training_df(scenario_configuration)
+
+    results_recorder = ResultsRecorder(scenario_configuration=scenario_configuration)
+
+    clock = ExternalClock(start_time=0)
+
+    container_mapping = await initialize_constant_bitrate_broadcast_agents(
+        scenario_configuration=scenario_configuration,
+        results_recorder=results_recorder,
+        clock=clock)
+
+    communication_network_entity = (
+        MetaModelScheduler(container_mapping=container_mapping,
+                           inet_installation_path='/home/malin/cocoon_omnet_workspace/inet4.5/src',
+                           simu5G_installation_path='/home/malin/PycharmProjects/trace/Simu5G-1.2.2/src',
+                           config_name=scenario_configuration.network_type.value,
+                           omnet_project_path='/home/malin/PycharmProjects/cocoon_DAI/cocoon_omnet_project/',
+                           in_training_mode=False,
+                           training_df=training_df,
+                           cluster_distance_threshold=scenario_configuration.cluster_distance_threshold.value,
+                           scenario_duration_ms=scenario_configuration.scenario_duration.value,
+                           i_pupa=scenario_configuration.i_pupa.value,
+                           output_file_name='results/cocoon_message_observations/production.csv'))
+    results_recorder.set_scheduler(communication_network_entity)
+    async with activate([c for c in container_mapping.values()]) as _:
+        results_recorder.start_scenario_recording()
+        await communication_network_entity.scenario_finished
+    results_recorder.stop_scenario_recording()
