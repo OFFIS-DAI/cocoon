313bde16747f46018329d4794fdf49f8957e55ff
Malin <malin.radtke@uni-oldenburg.de>
2025-07-01 08:30:48 +0200

    Clean up.

--- variable.py
+++
@@ -1,8 +0,0 @@
-import os.path
-from pathlib import Path
-
-path = Path(__file__).absolute()
-PATH = os.path.abspath(os.path.join(path, os.pardir))
-
-SIMULATION_DATA_FOLDERS = ['simulation_data']
-STATE_DATA_FOLDER = 'state_data'
