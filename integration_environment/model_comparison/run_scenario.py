e7645c1348a6af53ca7caf85cdbe60d907bfb3b4
Malin <malin.radtke@uni-oldenburg.de>
2025-07-11 13:08:44 +0200

    Add phase.

--- integration_environment/model_comparison/run_scenario.py
+++ integration_environment/model_comparison/run_scenario.py
@@ -7,9 +7,10 @@ from integration_environment.scenario_configuration import ScenarioConfiguration
 
 async def run_scenario_by_id(scenario_id: str):
     config = ScenarioConfiguration.from_scenario_id(scenario_id=scenario_id)
-    await run_scenario_config(scenario_configuration=config, run=config.run)
+    await run_scenario_config(scenario_configuration=config, run=config.run, phase=None)
 
 
 if __name__ == "__main__":
-    s_id = 'meta_model_training-two-medium-one_day-deer_use_case-simbench_lte450-none-none-none-none-none-0'
+    # meta_model-five-small-one_min-cbr_broadcast_1_mps-simbench_ethernet-half-ten-center-center-none-0
+    s_id = ('meta_model-five-small-one_min-cbr_broadcast_1_mps-simbench_5g-three-ten-center-center-none-0')
     asyncio.run(run_scenario_by_id(scenario_id=s_id))
