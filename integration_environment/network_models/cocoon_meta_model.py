dc20c901ac20ec270393b036d1cb660c5d400759
Malin <malin.radtke@uni-oldenburg.de>
2025-08-30 17:14:33 +0200

    Change back random state.

--- integration_environment/network_models/cocoon_meta_model.py
+++ integration_environment/network_models/cocoon_meta_model.py
@@ -373,7 +373,7 @@ class CocoonMetaModel:
         # make df smaller for clustering
         reduced_df = training_df.copy()
         reduced_df.drop_duplicates(inplace=True)
-        reduced_df = reduced_df.sample(n=1000, random_state=4)
+        reduced_df = reduced_df.sample(n=1000, random_state=42)
 
         # Identify non-constant features
         feature_vars = reduced_df[self.object_variables].var()
