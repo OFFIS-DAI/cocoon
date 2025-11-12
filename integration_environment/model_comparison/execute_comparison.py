e144a166ae43afa65f505dd048a6f11b92959df4
Malin <malin.radtke@uni-oldenburg.de>
2025-11-12 15:00:50 +0100

    Adjust hyper-params after analysis.

--- integration_environment/model_comparison/execute_comparison.py
+++ integration_environment/model_comparison/execute_comparison.py
@@ -197,11 +197,11 @@ def get_scenario_configurations_for_phase_2():
                                                           scenario_duration=scenario_duration,
                                                           traffic_configuration=traffic_config,
                                                           network_type=network,
-                                                          cluster_distance_threshold=ClusterDistanceThreshold.one,
-                                                          i_pupa=BatchSizeIPupa.fifty,
-                                                          learning_rate_weighting=LearningRateWeighting.large,
-                                                          butterfly_threshold_value=ButterflyThresholdValue.small,
-                                                          substitution_priority=SubstitutionPriority.error_level,
+                                                          cluster_distance_threshold=ClusterDistanceThreshold.three,
+                                                          i_pupa=BatchSizeIPupa.hundred,
+                                                          learning_rate_weighting=LearningRateWeighting.center,
+                                                          butterfly_threshold_value=ButterflyThresholdValue.center,
+                                                          substitution_priority=SubstitutionPriority.none,
                                                           test_train_split=tts))
                         else:
                             scenario_configurations.append(
